52f2d30e40b49d3f571d4b5ea8fa9a8087e587cd
Joe-Tianwei <joe.zhou@snaplii.com>
2023-11-09 11:24:35 -0500

    remove .env

--- models/todo.js
+++ models/todo.js
@@ -1,8 +1,37 @@
-import { Sequelize, DataTypes } from 'sequelize';
-
-const sequelize = new Sequelize('sqlite::memory:');
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/connection.js';
 
 const Todo = sequelize.define('Todo', {
-    username: DataTypes.STRING,
-    birthday: DataTypes.DATE,
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    todo_no: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    todo_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    todo_desc:{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    create_datetime:{
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    update_datetime:{
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    finished:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    }
 });
+
+export default Todo;
